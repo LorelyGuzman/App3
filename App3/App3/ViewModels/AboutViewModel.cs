5765258d90ba83673db915f883202d6902c9bc00
“LorelyGuzman” <viajero34210@innovaccion.mx>
2020-11-08 10:58:15 -0600

    App con botón que dirige a un chatbot

---
+++ App3/App3/ViewModels/AboutViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace App3.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "Inicio";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://webchat.botframework.com/embed/botdeapp-bot/gemini?b=botdeapp-bot&s=E3qLHxJSymI.xAq5AacZUisbEj9Mt6YZO8OFpHc99w29mHfOXyDgsz0&username=You"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
